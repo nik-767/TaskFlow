a980078458f67da8ae7cd8e477fc9b96fce80b49
nikhil <nikhilkalra124421@gmail.com>
2026-07-04 18:12:38 +0530

    removed the task app swtich to accouts only

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
 # Create your models here.
+
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
 
@@ -10,5 +16,31 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = ['email']  #if we remove username and add email we need to make custom Usermanager
 
     
+    
     def __str__(self):
         return self.username
+    
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+
+    bio = models.TextField(blank=True, null=True)
+
+    phone_number = PhoneNumberField(blank=True)
+
+    profile_image = models.ImageField(
+        upload_to="profile_images/",
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
--- accounts/signals.py
+++ accounts/signals.py
@@ -1,17 +1,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+
 from .models import Profile
-from django.contrib.auth import get_user_model
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def created_user(sender, instance , created , **kwargs):
 
-    User = get_user_model
 
     if created:
         Profile.objects.create(user=instance)
-    else:
-        if hasattr(instance, 'profile'):
-            instance.profile.save()
