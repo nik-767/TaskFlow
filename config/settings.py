69faf4f0880c3ad64bb8a83aad1ab5c2091eaab4
Nikhil Kalra <nikhilkalra124421@gmail.com>
2026-07-20 18:42:44 +0530

    addig docker & postgres intregation for db

--- config/settings.py
+++ config/settings.py
@@ -85,11 +85,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.environ.get("POSTGRES_DB", "taskflow"),
+    "USER": os.environ.get("POSTGRES_USER", "postgres"),
+    "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
+    "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+    "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+  }
 }
 
 
